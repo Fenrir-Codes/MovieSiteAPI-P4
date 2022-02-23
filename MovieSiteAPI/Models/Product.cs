98753773465c77895ced22ca30543e7d816d756c
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2022-02-23 14:09:27 +0100

    dialog as shopping cart

--- MovieSiteAPI/Models/Product.cs
+++ MovieSiteAPI/Models/Product.cs
@@ -9,7 +9,7 @@ namespace MovieSiteAPI.Models
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
+        public int Price { get; set; }
         public int AddDays { get; set; }
 
     }
